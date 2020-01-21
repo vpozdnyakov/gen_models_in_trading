d299f79189790d93cfa37ecb9862febe619cec8b
Vitaliy Pozdnyakov <vitalijpozdnakov@MacBook-Pro-Vitalij-2.local>
2020-01-22 02:39:39 +0300

    upd

---
+++ simulations/random_walk.R
@@ -0,0 +1,33 @@
+#' ---
+#' title: "Limit of random walk"
+#' author: "Vitaliy Pozdnyakov"
+#' date: "January 2020"
+#' output: pdf_document
+#' ---
+
+# ex1.07.R
+set.seed(1)
+n <- 10
+T <- 1
+t <- seq(0, 1, length=100)
+S <- cumsum(2*(runif(n)>0.5)-1)
+W <- numeric(100)
+for(i in (2:100))
+  W[i] <- ifelse(t[i]*n > 0, S[t[i]*n] / sqrt(n), 0)
+palette(rainbow(6))
+plot(t, W, type = "l", ylim = c(-1, 1), col=palette()[1])
+
+n <- 100
+S <- cumsum(2*(runif(n)>0.5)-1)
+W <- numeric(100)
+for(i in (2:100))
+  W[i] <- ifelse(t[i]*n > 0, S[t[i]*n] / sqrt(n), 0)
+lines(t, W, col=palette()[5])
+
+n <- 1000
+S <- cumsum(2*(runif(n)>0.5)-1)
+W <- numeric(100)
+for(i in (2:100))
+  W[i] <- ifelse(t[i]*n > 0, S[t[i]*n] / sqrt(n), 0)
+lines(t, W, col=palette()[3])
+
